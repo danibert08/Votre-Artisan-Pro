201b7ec4e31afc9a1cc69c46d2271e52417e5d07
danibert08 <berthida34@gmail.com>
2026-03-01 10:58:02 +0100

    adding dynamical canonical link

--- public/pages_artisans/la-belle-peinture/index.php
+++ public/pages_artisans/la-belle-peinture/index.php
@@ -12,7 +12,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="canonical" href="https://la-belle-peinture.votreartisanpro.fr/" />
+    <?php 
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+        $host = $_SERVER['HTTP_HOST'];
+        $canonical_url = $protocol . "://" . $host . "/";
+    ?>
+    <link rel="canonical" href="<?= $canonical_url; ?>" />
     <title>La Belle Peinture : travaux de peinture et de vitrerie</title>
     <link rel="stylesheet" href="/pages_artisans/css/reset.css" class="css">
     <link rel="stylesheet" href="/pages_artisans/css/style.css" class="css">
