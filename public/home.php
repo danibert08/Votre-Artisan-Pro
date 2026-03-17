50d6b7f7bb44d0ac1b9342aaa561724bb263d566
danibert08 <berthida34@gmail.com>
2026-03-17 22:39:34 +0100

    update models link

--- public/home.php
+++ public/home.php
@@ -52,31 +52,31 @@
                                 <!--     Main1 Pictures     -->
                     
                     <p class="services__lambda_photos">
-                        <a class="theme_link" href="https://espaces-verts">Feuillage<br><img class="photo photo1" src="/images_vap/photos1/m1.jpeg"  alt=""></a>
-                        <a class="theme_link" href="https://ma-salle-de-bain">Lilas<br><img class="photo photo2" src="/images_vap/photos1/m2.jpeg"  alt=""></a> 
+                        <a class="theme_link" href="https://espaces-verts.votreartisanpro.fr">Feuillage<br><img class="photo photo1" src="/images_vap/photos1/m1.jpeg"  alt=""></a>
+                        <a class="theme_link" href="https://ma-salle-de-bain.votreartisanpro.fr">Lilas<br><img class="photo photo2" src="/images_vap/photos1/m2.jpeg"  alt=""></a> 
                     </p>
                     
                     <p class="services__lambda_photos">
-                        <a class="theme_link" href="https://martine-beauty">Flamant<br><img class="photo photo3" src="/images_vap/photos1/m3.jpeg"  alt=""></a>                    
-                        <a class="theme_link" href="https://mon-cordonnier">Cuir<br><img class="photo photo4" src="/images_vap/photos1/m4.jpeg" alt=""></a>
+                        <a class="theme_link" href="https://martine-beauty.votreartisanpro.fr">Flamant<br><img class="photo photo3" src="/images_vap/photos1/m3.jpeg"  alt=""></a>                    
+                        <a class="theme_link" href="https://mon-cordonnier.votreartisanpro.fr">Cuir<br><img class="photo photo4" src="/images_vap/photos1/m4.jpeg" alt=""></a>
                     </p>
                     <p class="services__lambda_photos">
-                        <a class="theme_link" href="https://mon-electricien">Ciel<br><img class="photo photo5" src="/images_vap/photos1/m5.jpeg" alt=""></a>                    
-                        <a class="theme_link" href="https://mon-menuisier">Terre<br><img class="photo photo6" src="/images_vap/photos1/m6.jpeg" alt=""></a>
+                        <a class="theme_link" href="https://mon-electricien.votreartisanpro.fr">Ciel<br><img class="photo photo5" src="/images_vap/photos1/m5.jpeg" alt=""></a>                    
+                        <a class="theme_link" href="https://mon-menuisier.votreartisanpro.fr">Terre<br><img class="photo photo6" src="/images_vap/photos1/m6.jpeg" alt=""></a>
                     </p>
                   
                     <p class="services__lambda_photos">
-                        <a class="theme_link" href="https://mon-parpaing">Soleil<br><img class="photo photo7" src="/images_vap/photos1/m7.jpeg" alt=""></a>                  
-                        <a class="theme_link" href="https://mon-peintre">Aubergine<br><img class="photo photo8" src="/images_vap/photos1/m8.jpeg" alt=""></a>
+                        <a class="theme_link" href="https://mon-parpaing.votreartisanpro.fr">Soleil<br><img class="photo photo7" src="/images_vap/photos1/m7.jpeg" alt=""></a>                  
+                        <a class="theme_link" href="https://mon-peintre.votreartisanpro.fr">Aubergine<br><img class="photo photo8" src="/images_vap/photos1/m8.jpeg" alt=""></a>
                     </p>
                    
                     <p class="services__lambda_photos">
-                        <a class="theme_link" href="https://mon-platrier">Lagon<br><img class="photo photo9" src="/images_vap/photos1/m9.jpeg" alt=""></a>                   
-                        <a class="theme_link" href="https://mon-serrurier">Océan<br><img class="photo photo10" src="/images_vap/photos1/m10.jpeg" alt=""></a>
+                        <a class="theme_link" href="https://mon-platrier.votreartisanpro.fr">Lagon<br><img class="photo photo9" src="/images_vap/photos1/m9.jpeg" alt=""></a>                   
+                        <a class="theme_link" href="https://mon-serrurier.votreartisanpro.fr">Océan<br><img class="photo photo10" src="/images_vap/photos1/m10.jpeg" alt=""></a>
                     </p>
                     <p class="services__lambda_photos">
-                        <a class="theme_link" href="https://mon-ferronnier">Souris<br><img class="photo photo11" src="/images_vap/photos1/m11.jpeg" alt=""></a>                                        
-                        <a class="theme_link" href="https://mon-verrier">Tomate<br><img class="photo photo12" src="/images_vap/photos1/m12.jpeg" alt=""></a>                                        
+                        <a class="theme_link" href="https://mon-ferronnier.votreartisanpro.fr">Souris<br><img class="photo photo11" src="/images_vap/photos1/m11.jpeg" alt=""></a>                                        
+                        <a class="theme_link" href="https://mon-verrier.votreartisanpro.fr">Tomate<br><img class="photo photo12" src="/images_vap/photos1/m12.jpeg" alt=""></a>                                        
                     </p>
                     <p class="services__lambda_photos">
                                                               
