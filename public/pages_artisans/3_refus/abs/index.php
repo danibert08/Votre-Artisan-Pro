fc5e54b8cbb107da7b8e42dc56027fa65f53a7fe
danibert08 <berthida34@gmail.com>
2026-03-26 11:00:42 +0100

    delete unused file

--- public/pages_artisans/3_refus/abs/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require('base.php');
-    require(__DIR__ .'/../2_templates/template.php');
-?>
--- public/pages_artisans/3_refus/abs/mentions_legales.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-   require('base.php');
-    require(__DIR__ .'/../2_templates/template_mentions_legales.php');
-    ?>
-
-
